a12517f6e6213fe5edd49acb99a4c6086dbfc34f
Walter Mitchell <walter.lee.mitchell@gmail.com>
2026-07-25 16:11:03 -0700

    Added multiplication implementation

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -6,4 +6,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@i
+M=1
+@R2
+M=0
+(LOOP)
+   @R1
+   D=M
+   @i
+   D=M-D
+   @STOP
+   D;JGT // if i > n, goto STOP; aka if i-n >= 0
+
+   @R0
+   D=M
+   @R2
+   M=D+M // add R0 to the product
+
+   @i
+   M=M+1 // add one to i
+
+   @LOOP
+   0;JMP // goto LOOP
+
+(STOP)
+   @STOP
+   0;JMP
