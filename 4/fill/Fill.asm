9b330883217fb957ec7c77e945730e194b9b85d7
Walter Mitchell <walter.lee.mitchell@gmail.com>
2026-07-27 16:14:10 -0700

    Simple screen fill loop added

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -6,7 +6,44 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
+@KBD
+D=A
+@temp
+M=D
+@SCREEN
+D=A
+@temp2
+M=D
 
-(BLACKEN)
+@temp2
+D=M
+@temp
+D=M-D
+
+@n
+M=D // n = KBD-SCREEN
+
+@UPDATE_SCREEN
+0;JMP
+(UPDATE_SCREEN)
+    @n
+    D=M
+    @i
+    D=M-D
+    @END
+    D;JEQ // jump if i == n
+
     @SCREEN
-    
+    D=A
+    @i
+    A=D+M // get the address of the current register
+    M=-1
+
+    @i
+    M=M+1 // i+=1
+
+    @UPDATE_SCREEN
+    0;JMP
+(END)
+    @END
+    0;JMP
