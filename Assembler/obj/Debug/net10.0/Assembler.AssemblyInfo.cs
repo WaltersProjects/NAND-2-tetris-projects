8b9e8988c0a58b5f9c51512eb361048deff61018
Walter Mitchell <walter.lee.mitchell@gmail.com>
2026-08-07 15:59:36 -0700

    Added Parser.cs to cleanify parsing logic

--- Assembler/obj/Debug/net10.0/Assembler.AssemblyInfo.cs
+++ Assembler/obj/Debug/net10.0/Assembler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assembler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da734900d47f7a70b642a159dd36b424c15be78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0970e141317275104fa93f1957fbf9f6dc1e7542")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assembler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assembler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
